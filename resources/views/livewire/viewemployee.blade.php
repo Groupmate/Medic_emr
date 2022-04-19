a2328f799ddb349cb6a5c877c6da2e98b7d75583
naty466 <102832839+naty466@users.noreply.github.com>
2022-04-19 21:27:00 +0300

    Create viewemployee.blade.php

---
+++ resources/views/livewire/viewemployee.blade.php
@@ -0,0 +1,26 @@
+<table class="min-w-full leading-normal">
+        <thead class="text-purple-500">
+            <tr >
+                <th  class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Name center</th>
+                <th  class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">manager</th>
+                <th  class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">type</th>
+                <th   class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">region</th>
+                <th   class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">city_name</th>
+                <th   class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">action</th>
+
+            </tr>
+        </thead>
+        <tbody>
+                    <!-- @foreach($employes as $organ)
+                                                    <tr>
+                                                        <td   class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->first_name}}</td>
+                                                        <td  class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->last_name}}</td>
+                                                        <td   class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->type}}</td>
+                                                        <td   class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->image}}</td>
+                                                        <td  class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->address}}</td>
+                                                        <td  class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->phone}}</td>
+                                                        <td  class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{$organ->age}}</td>
+                                            </tr>
+                        @endforeach -->
+        </tbody>
+</table>
