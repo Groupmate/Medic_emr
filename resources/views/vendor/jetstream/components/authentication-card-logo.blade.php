46c0563cb6eb9537a27d1c4c704302059a4a9dcf
Natnael Tefera <49879895+Naty-Tefera@users.noreply.github.com>
2022-04-23 12:15:48 +0300

    changes on web routing

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,8 +1,5 @@
 <a href="/">
     <img class="" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/light-with-button-svg3.svg" alt="logo" />
-
-      
-
         <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
         <i class="fad fa-list-ul"></i>
 </a>
--- routes/web.php
+++ routes/web.php
@@ -2,52 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/create_regionalbureau', function () {
-        return view('federal.create_regionalbureau');
-    })->name('create_regionalbureau');
-    Route::get('/create_manager', function () {
-        return view('federal.create_manager');
-    })->name('create_manager');
-    Route::get('/profile', function () {
-        return view('federal.profile');
-    })->name('create_regionalbureau');
-
-
-    Route::get('/employee', function () {
-        return view('HR.employee');
-    })->name('employee');
-
-    Route::get('/viewemployee', function () {
-        return view('HR.viewemployee');
-    })->name('viewemployee');
-
-});
-Route::get('/dashboard', 'App\Http\Controllers\HomeController@index')->name('dashboard');
 Route::redirect(uri:'/', destination:'login');
-Route::middleware(['auth:sanctum', 'verified'])->get('/patient', function () {
-    return view('doctors.patient');
-})->name('patient');
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::view('/home', 'doctors.home')->name('doctors.home');
+Route::get('/dashboard', 'App\Http\Controllers\HomeController@index')->name('dashboard');
+
+Route::middleware([ 'auth:sanctum',config('jetstream.auth_session'), 'verified'
+    ])->group(function () {
+        Route::get('/create_regionalbureau', function () { return view('federal.create_regionalbureau'); })->name('create_regionalbureau');
+        Route::get('/create_manager', function () {  return view('federal.create_manager');})->name('create_manager');
+        Route::get('/profile', function () { return view('federal.profile'); })->name('create_regionalbureau');
+        Route::get('/employee', function () { return view('HR.employee');})->name('employee');
+        Route::get('/viewemployee', function () {  return view('HR.viewemployee');})->name('viewemployee');
+        Route::get('/patient', function () { return view('doctors.patient'); })->name('patient');
+        Route::get('/home', function () { return view('doctors.home'); })->name('doctors.home');   
 });
