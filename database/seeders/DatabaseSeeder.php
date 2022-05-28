0bae7236a2ed883d12b52df632f1985af7fb6daf
Natnael Tefera <49879895+Naty-Tefera@users.noreply.github.com>
2022-05-28 14:04:15 +0300

    Data Seeder For Some Models
    ->Patient
    ->Appointment
    ->Medical_data
    ->Medical_drug
    ->Hospitals

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = \Faker\Factory::create();
+
+        for ($loop = 0; $loop < 10; $loop++) {
+            DB::table("users")->insert([
+               
+                'organization_id'=>$faker->numberBetween(1,10),
+                'first_name'=>$faker->name(),
+                'last_name'=>$faker->name(),
+                'phone'=>$faker->numberBetween(251900000000,251999999999),
+                'email'=>$faker->email(),
+                'role'=>$faker->numberBetween(1,5),
+                'password'=>encrypt('password'),
+                'address'=>$faker->name(),
+                'sex'=>$faker->randomElement(["Male","Female"]),
+                'date_of_birth'=>$faker->name(),
+                'profile_pic'=>$faker->name(),
+                'created_at'=>$faker->dateTimeBetween("-6 month "," +1 month" ),
+                'updated_at'=>$faker->name(),
+
+
+            ]);
+        }
         // \App\Models\User::factory(10)->create();
     }
 }
---
+++ database/seeders/Medical_drugSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class Medical_drugSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($loop = 0; $loop < 150; $loop++) {
+
+            DB::table("medical_datas")->insert([
+                "hospital_id"=>$faker->numberBetween(1,150),
+                "name"=>$faker->name(),
+                "department"=>$faker->randomElement(["Radiology","General Doctor","Eye","Mind","Skeleton","Nerve"]),
+                "quantity"=>$faker->numberBetween(1,150),
+                "expired_date"=>$faker->date(),
+                "photo"=>$faker->word(),	
+                "description"=>$faker->randomElement(["2015, there were about 212 million reported cases of malaria in the world","he reality is that diarrheal diseases are the number one killer of children under five in the world"]), 
+                "price"=>$faker->numberBetween(150,5500),
+                "created_at	"=>$faker->date(),
+                "updated_at"=>$faker->date(),
+            ]);
+        }
+        //
+    }
+}
