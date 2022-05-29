a1148a22036e4c1653b1dbf124b11ebad9fbf6d5
Natnael Tefera <49879895+Naty-Tefera@users.noreply.github.com>
2022-05-29 22:51:18 +0300

    Changes on Seeder

--- database/seeders/Medical_drugSeeder.php
+++ database/seeders/Medical_drugSeeder.php
@@ -20,16 +20,17 @@ class Medical_drugSeeder extends Seeder
 
         for ($loop = 0; $loop < 150; $loop++) {
 
-            DB::table("medical_datas")->insert([
+            DB::table("medical_drugs")->insert([
                 "hospital_id"=>$faker->numberBetween(1,150),
                 "name"=>$faker->name(),
-                "department"=>$faker->randomElement(["Radiology","General Doctor","Eye","Mind","Skeleton","Nerve"]),
+                "type"=>$faker->name(),
+               "department"=>$faker->randomElement(["Radiology","General Doctor","Eye","Mind","Skeleton","Nerve"]),
                 "quantity"=>$faker->numberBetween(1,150),
                 "expired_date"=>$faker->date(),
                 "photo"=>$faker->word(),	
                 "description"=>$faker->randomElement(["2015, there were about 212 million reported cases of malaria in the world","he reality is that diarrheal diseases are the number one killer of children under five in the world"]), 
                 "price"=>$faker->numberBetween(150,5500),
-                "created_at	"=>$faker->date(),
+                "created_at"=>$faker->date(),
                 "updated_at"=>$faker->date(),
             ]);
         }
