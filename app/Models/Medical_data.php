ec2c65164dd7312300fc47b94eb0efec8fa17e5d
Natnael Tefera <49879895+Naty-Tefera@users.noreply.github.com>
2022-05-29 23:01:13 +0300

    Update on Medical_data.php

--- app/Models/Medical_data.php
+++ app/Models/Medical_data.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<<< HEAD:app/Models/Medical_data.php
 class Medical_data extends Model
-========
-class Discharge extends Model
->>>>>>>> medic_aemr:app/Models/Discharge.php
 {
     use HasFactory;
 }
