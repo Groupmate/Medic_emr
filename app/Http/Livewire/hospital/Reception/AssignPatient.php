01c17e5fefc793fa3d72cca6784a96f27dcfb6a8
seretseCSE <60090188+seretseCSE@users.noreply.github.com>
2022-05-31 04:44:41 -0300

    Revert "discharge message"
    This reverts commit 1e8526585b8f1102cd561cbef9b99949a057ed4a.

--- app/Http/Livewire/hospital/Reception/AssignPatient.php
+++ app/Http/Livewire/hospital/Reception/AssignPatient.php
@@ -12,137 +12,27 @@ use App\Models\User;
 use Carbon\Carbon;
 
 class AssignPatient extends Component
-{    
+{
     use WithPagination;
-    use WithFileUploads;
 
-    public $perpage=5;
-    public $search='';
-    //  public $patients;
-   
+    public $patient_id, $user_id, $doctors = [], $users = [], $available_doctors, $patients,
+    $hospital_a,$status;
     public $modelId;
-    public $modalFormVisible=false;
-    public $modalEditFormVisible=false;
+    public $modalFormVisible = false;
 
-    public $firstname, $lastname, $phone_no,  $profile_pic,  $patient_id, $data_of_birth, $email,
-    $kebele, $zone, $woreda, $region, $cityname, $middelname, $sex;
-    public $user_id, $doctors=[], $available_doctors;
-    
+    public function mount(Patient $patients)
+    {
+        $this->patients = $patients;
+    }
+
     /**
-     * deleteShowModel
+     * createShowModal show modal
      *
-     * @param  mixed $id
      * @return void
      */
-    
-
-
-    public function updateShowModal($id)
+    public function createShowModal($id)
     {
         $this->modelId = $id;
-        $this->modalEditFormVisible=true;
-         $this->loadModel();
-    }
-
-   
-    /**
-     * update
-     *
-     * @return void
-     */
-    public function update()
-    {
-        $this->validate();
-        Patient::find($this->modelId)->update($this->modelData());
-        $this->modalEditFormVisible= false;
-        // $this->resetvars();
-    }    
-    /**
-     * modelData
-     *
-     * @return void
-     */
-    public function modelData()
-    {
-        return [
-            'firstname'=>$this->firstname,
-            'lastname'=>$this->lastname,
-            'sex'=>$this->sex,
-            'middelname'=>$this->middelname,
-            'patient_id'=>$this->patient_id,
-            'phone_no'=>$this->phone_no,
-            'email'=>$this->email,
-           
-            'profil_pic'=>$this->profile_pic,
-            'data_of_birth'=>$this->data_of_birth,
-      
-            
-            'region'=>$this->region,
-            'zone'=>$this->zone,
-            'woreda'=>$this->woreda,
-            'kebele'=>$this->kebele,
-            'cityname'=>$this->cityname,
-        ];
-    }       
-    /**
-     * rules validation
-     *
-     * @return void
-     */
-    public function rules()
-    {
-        return [
-            'firstname'=>'required',
-            'lastname'=>'required',
-            'middelname'=>'required',
-            'region'=>'required',
-            'profile_pic'=>'required',
-            'patient_id'=>'required',
-            // 'date_of_birth'=>'required',
-            'woreda'=>'required',
-            
-        ];
-    }     
-    /**
-     * loadModel
-     *
-     * @return void
-     */
-    public function loadModel()
-    {
-        $patient=Patient::find($this->modelId);
-            $this->firstname= $patient->firstname;
-            $this->lastname= $patient->lastname;
-            $this->middelname= $patient->middelname;
-            $this->patient_id= $patient->patient_id;
-            $this->phone_no= $patient->phone_no;
-            $this->email= $patient->email;
-         
-            $this->region= $patient->region;
-            $this->zone= $patient->zone;
-            $this->woreda= $patient->woreda;
-            $this->kebele= $patient->kebele;
-            $this->cityname= $patient->cityname;
-            $this->sex= $patient->sex;
-       
-            $this->data_of_birth=$patient->data_of_birth;
-            $this->profile_pic= $patient->profile_pic;
-           
-           
-           
-
-    }
-    /**
-     * createShowModal
-     *
-     * @return void
-     */
-    public function createShowModal()
-    {
-        $this->resetValidation();
-        $this->reset();
-        $date = Carbon::now();
-        $date = date('l');
 
         $doctor = Doctor::all();
         foreach($doctor as $doctor)
@@ -150,34 +40,55 @@ class AssignPatient extends Component
             $shift = $doctor->shift;
             foreach($shift as $shift)
             {
-                if($shift == $date)
+                if($shift == date('l'))
                 {
-                    $this->doctors[] = Doctor::where('id', $doctor->id)->first();
+                    $this->doctors[] = Doctor::where('id', $doctor->id)->pluck('user_id')->toArray();
                 }
             }
         }
-        
+        foreach($this->doctors as $doctor)
+        {
+            $this->users[] = User::where('id', $doctor)->first();
+        }
+
         $this->modalFormVisible=true;
+    }
+
+    public function rules()
+    {
+        return[
+            'user_id' => 'required',
+        ];
+    }
+
+    public function modeldata()
+    {
+        $this->patient_id=$this->modelId;
+
+        $a = Auth()->user()->id;
+        $hospital_a = Employee::where('user_id', $a)->first()->hospital_id;
+       // dd($hospital_a);
+        return[
+            'user_id'=> $this->user_id,
+            'status'=> "appointed",
+            'hospital_id'=> $hospital_a,
+            'patient_id'=> $this->patient_id,
+        ];
     }
 
     public function create()
     {
-        dd($this->user->id);
+        $this->validate();
+        $psw = Patient_Waiting_List::create($this->modeldata());
+        
+        $this->modalFormVisible=false;
     }
-    
-    // public function render()
-    // { 
-    //     $users = User::all();
-
-    //     return view('livewire.hospital.reception.assign-patient',  [
-    //         $this->patients = Patient::all(),$this->doctors,
-    //     ])->with('users', $users);
-    // }
     public function render()
     {
-        $users = User::all();
-        return view('livewire.hospital.reception.assign-patient')->with(['patients'=>patient::orderBy('id','DESC')->when($this->search,function($query,$search){
-            return $query->where('firstname','LIKE',"%$search%");
-        })->paginate($this->perpage)],$this->doctors)->with('users', $users);
+        $patients = Patient::all();
+
+        return view('livewire.hospital.reception.assign-patient',[
+            $this->patients = Patient::all(),$this->users
+        ]);
     }
 }
