2794bfafec4f7da1aee0537ed14989424621630b
seretseCSE <60090188+seretseCSE@users.noreply.github.com>
2022-06-11 09:21:10 -0300

    medical data generate

--- app/Http/Livewire/hospital/Doctor/Generatemedicaldata.php
+++ app/Http/Livewire/hospital/Doctor/Generatemedicaldata.php
@@ -12,21 +12,11 @@ class Generatemedicaldata extends Component
     {
         $this->patient = Patient::where('id', $id)->first(); 
     } 
-    
-    public function save()
-    {
-<<<<<<< Updated upstream
-        // dd('one');
-        return view('livewire.hospital.doctor.generatemedicaldata');
-=======
-        dd($patient->id);
-    }
-
+     
     public function render()
     {     
         return view('livewire.hospital.doctor.generatemedicaldata', [
             'patient' => $this->patient,
-        ]); 
->>>>>>> Stashed changes
+        ]);  
     }
 }
