815787bbbcac88847e718ca111c39a6e78f396da
sintayehuCSE <59802123+sintayehuCSE@users.noreply.github.com>
2022-06-12 08:57:28 +0300

    Refer Patient part two (2)
    Now you can refer  a patient to particular a hospital

--- app/Http/Livewire/hospital/Doctor/ReferPatient.php
+++ app/Http/Livewire/hospital/Doctor/ReferPatient.php
@@ -8,6 +8,7 @@ use App\Models\Patient_Waiting_List;
 use App\Models\Hospital;
 use App\Models\User;
 use App\Models\Doctor;
+use App\Models\Referal;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 use Livewire\WithPagination;
@@ -15,9 +16,52 @@ use Illuminate\Support\Facades\DB;
 
 class ReferPatient extends Component
 {
+    use WithPagination;
+    public $modelFormVisible, $modelConfirmDeleteVisible, $modelUpdateVisible;
+    public $patient_id, $date, $diagnosis, $refered_by, $refered_to;
+    public $modelId;
+
+    public function referShowModal($patient_id, $refered_by){
+        // $this-resetValidation();
+        // $this-reset();
+        $this->modelFormVisible = true;
+        $this->patient_id = $patient_id;
+        $this->refered_by = $refered_by;
+    }
+
+    public function modelData()
+    {
+        return [
+            'patient_id'=>$this->patient_id,
+            'date'=>$this->date,
+            'diagnosis'=>$this->diagnosis,
+            'refered_by'=>$this->refered_by,
+            'refered_to'=>$this->refered_to,
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            'patient_id'=>'required',
+            'date'=>'required',
+            'diagnosis'=>'required',
+            'refered_by'=>'required',
+            'refered_to'=>'required',
+        ];
+    }
+
+    public function refer(){
+        $this->validate();
+        $refer = Referal::create($this->modelData());
+        $this->modelFormVisible = false;
+        $this->reset();
+    }
+
     public function render()
     {
-        // $organization =Patient_Waiting_List::latest()->paginate(5);
+        $hospitals = DB::select('select * from hospitals');
+        // dd($hospitals);
         $userID  =DB::select('select * from users where id = ?', [Auth::user()->id]); 
         // dd($userID);
     
@@ -26,6 +70,6 @@ class ReferPatient extends Component
         $patientID = DB::select('select * from patients');
         // dd($patientID);
         return view('livewire.hospital.doctor.refer-patient', 
-        ['userID'=>$userID, 'assignmentDoctor'=>$assignmentDoctor, 'patientID'=>$patientID]);
+        ['userID'=>$userID, 'hospitals'=>$hospitals, 'assignmentDoctor'=>$assignmentDoctor, 'patientID'=>$patientID]);
     }
 }
--- resources/views/livewire/hospital/doctor/refer-patient.blade.php
+++ resources/views/livewire/hospital/doctor/refer-patient.blade.php
@@ -1,47 +1,95 @@
-<div class = "ml-6 pt-80">
-    The whole world belongs to you. 
-    <div class="border-b border-gray-400 shadow">
+<div class = "ml-6 pt-6 bg-black ">
+    <!-- The whole world belongs to you.  -->
+    <div class="border-b pt-5 border-gray-400 shadow">
         <table class="table-auto">
             <thead class="text-purple-500">
                 <tr >
-                    <th  class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Patient ID</th>
-                    <th  class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">First Name</th>
-                    <th   class="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Second Name</th>
-                    <th   class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Last Name</th>
-                    <th   class="px-56 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Action</th>
+                    <th  class="px-6 py-3 border-b-2 border-gray-500 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Patient ID</th>
+                    <th  class="px-6 py-3 border-b-2 border-gray-500 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">First Name</th>
+                    <th   class="px-6 py-3 border-b-2 border-gray-500 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Second Name</th>
+                    <th   class="px-5 py-3 border-b-2 border-gray-500 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Last Name</th>
+                    <th   class="px-56 py-3 border-b-2 border-gray-500 bg-gray-100 text-left text-xs font-semibold text-purple-500 uppercase tracking-wider">Action</th>
 
                 </tr>
             </thead>
-            <tbody>
-                <tr >
-                    
+            <tbody>                                 
                     @foreach($assignmentDoctor as $organ)
                         @foreach($userID as $userId)
                             @foreach($patientID as $patientId)
-                                @if($userId -> id == $organ->user_id && $organ->patient_id == $patientId->id)
-                                        <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->id }}</td>
-                                        <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->first_name }}</td>
-                                        <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->middle_name }}</td>
-                                        <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->last_name }}</td>
-                                        <td>
-                                            <x-jet-button class="bg-blue-500 ml-6 " wire:click="referShowModal({{ $patientId->id }})">
-                                                {{ __('Refer Patient') }}
-                                            </x-jet-button>
-                                            <x-jet-button class="bg-green-500 ml-6"  wire:click="updateReferShowModal({{ $patientId->id }})">
-                                                {{ __('update Refer') }}
-                                            </x-jet-button>
-                                            <x-jet-danger-button class="bg-red-500 ml-6" wire:click="deleteShowModal({{ $patientId->id }})" >
-                                                {{ __('Delete Refer') }}
-                                            </x-jet-button>
-                                        </td>
-                                @endif
+                                <tr>
+                                    @if($userId -> id == $organ->user_id && $organ->patient_id == $patientId->id)
+                                            <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->id }}</td>
+                                            <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->first_name }}</td>
+                                            <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->middle_name }}</td>
+                                            <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">{{ $patientId->last_name }}</td>
+                                            <td class="px-6 py-3 border-b border-gray-200 bg-white text-sm">
+                                                <x-jet-button class="bg-blue-500 ml-6 " wire:click="referShowModal({{ $patientId->id }}, {{$userId->id}})">
+                                                    {{ __('Refer Patient') }}
+                                                </x-jet-button>
+                                                <x-jet-button class="bg-green-500 ml-6"  wire:click="updateReferShowModal({{ $patientId->id }})">
+                                                    {{ __('update Refer') }}
+                                                </x-jet-button>
+                                                <x-jet-danger-button class="bg-red-500 ml-6" wire:click="deleteShowModal({{ $patientId->id }})" >
+                                                    {{ __('Delete Refer') }}
+                                                </x-jet-button>
+                                            </td>
+                                    @endif
+                                </tr>                    
                             @endforeach
                         @endforeach
-                    @endforeach
-                </tr>
-                
+                    @endforeach                                             
             </tbody> 
         </table>
-    </div>
-   
+    </div>  
+
+                 {{-- Modal form for refering each patient goes here --}}
+
+    <x-jet-dialog-modal wire:model="modelFormVisible">
+        <x-slot name="title">
+            {{ __('Refer Patient') }} {{$modelId}}
+        </x-slot>
+
+        <x-slot name="content">
+
+            <div class="mt-4">
+                <x-jet-label for="date" value="{{ __('Refered Date') }}" />
+                <x-jet-input id="date" class="block p-6  mt-1 w-1/2 bg-gray-500 border-2 border-gray-500" type="date" name="date" wire:model.debounce.800ms="date" />
+                @error('date') <span class="error text-red-600">{{$message}}</span>@enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="Refere_to" value="{{ __('Refered To') }}" />
+                <select class="block mt-1 w-1/2 p-6  border-2 bg-gray-500  border-gray-500 " wire:model.debounce.800ms="refered_to" />
+                :value="old('refered_to')" required autofocus autocomplete="refered_to">
+                    <option selected >---Select Hospital---</option>
+                    @forelse ($hospitals as $hospital)
+                        <option value= {{ $hospital->id }} > {{ $hospital->name }} </option>
+                    @empty
+                        <option disabled>No Hospital to Refer to</option>
+                    @endforelse
+                </select>
+                @error('refered_to') <span class="error text-red-600">{{$message}}</span>@enderror
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="diagnosis Result" value="{{ __('Diagnosis Result') }}" />
+                <x-jet-input id="diagnosis" class="block p-6 mt-1 w-1/2 bg-gray-500  border-2 border-gray-500 text-left text-xl font-semibold" type="text" name="diagnosis" wire:model.debounce.800ms="diagnosis" />
+                @error('diagnosis') <span class="error text-red-600">{{$message}}</span>@enderror
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            @if($modelId)
+                <x-jet-secondary-button class="bg-green-500" wire:click="update" wire:loading.attr="disabled">
+                    {{ __('update') }}
+                </x-jet-secondary-button>
+            @else
+                <x-jet-secondary-button class="bg-green-500" wire:click="refer" wire:loading.attr="disabled">
+                    {{ __('Refer Patient') }}
+                </x-jet-secondary-button>
+            @endif
+
+            <x-jet-secondary-button class="ml-3" wire:click="$toggle('modelFormVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+ 
 </div>
