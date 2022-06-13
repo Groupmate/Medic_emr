8e33bd14fd3ae1907d045e70c4ccc973fcd9d6e5
naty466 <102832839+naty466@users.noreply.github.com>
2022-06-13 13:51:21 +0300

    kkk

--- app/Http/Livewire/hospital/pharmacy/IssueDrug.php
+++ app/Http/Livewire/hospital/pharmacy/IssueDrug.php
@@ -9,6 +9,11 @@ class IssueDrug extends Component
 {
     public $search='',$patients;
     
+    public function search()
+    {
+        $patients = Patient::where('firstname',$search)->first()->id;
+        dd($this->patients);
+    }
     public function render()
     {
         $prescirbed =Prescribe_drug::where('patient_id',$this->patients)->first();
