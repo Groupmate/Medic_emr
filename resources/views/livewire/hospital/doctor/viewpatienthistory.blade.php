4d8164c5b4b5d973193ef95dc3edae89b4483feb
Natnael Tefera <49879895+Naty-Tefera@users.noreply.github.com>
2022-06-20 02:35:31 +0300

    Update on Viewpatienthistory

--- resources/views/livewire/hospital/doctor/viewpatienthistory.blade.php
+++ resources/views/livewire/hospital/doctor/viewpatienthistory.blade.php
@@ -1,14 +1,28 @@
-<div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-1">
+<div class="p-5 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-1">
   @forelse ($medicaldata as $medicaldata) 
     <div class="rounded overflow-hidden shadow-lg">
        
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">  Dignosis Information </div> 
-                <p class="text-gray-700 text-base">
-                @foreach ($medicaldata->diagnosis_info as $diagnosis_info)
-                    {{ $diagnosis_info }}
+            <div class="font-bold text-xl mb-2">  Dignosis Information 
+               @ <div class="mr-2">
+                 @foreach ($medicaldata->created_at as $created_at)
+                    {{ $created_at }}
                 @endforeach
-                </p>
+               </div>
+            
+            </div> 
+            <p class="text-gray-700 text-base">
+            @foreach ($medicaldata->diagnosis_info as $diagnosis_info)
+                {{ $diagnosis_info }}
+            @endforeach
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black-700 mr-2 mb-2">
+              @foreach ($medicaldata->symptom as $symptom)
+                {{ $symptom }}
+              @endforeach
+            </span>
         </div>
         <div class="px-6 pt-4 pb-2">
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black-700 mr-2 mb-2">
